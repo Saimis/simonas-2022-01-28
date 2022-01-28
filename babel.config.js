09b9714f9af85e2a63d519eb8bb27b4cacdae4bb
Simonas Belickas <simonas.belickas@gmail.com>
2022-01-28 16:07:21 +0200

    dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
+const environment = process.env.ENVIRONMENT || 'development';
+require('dotenv').config({path: path.join(__dirname, `.env.${environment}`)});
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    'transform-inline-environment-variables',
     [
       'module-resolver',
       {
---
+++ src/types/processVars.d.ts
@@ -0,0 +1,7 @@
+declare var process: {
+  env: {
+    NODE_ENV: string;
+    CRYTPTOFACILITIES_URL: string;
+    ENVIRONMENT: string;
+  };
+};
